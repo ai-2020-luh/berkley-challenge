bac9e8ac4ba80bb1cfeb9e6bacdcd980deec5a59
Nils Martel <nils.martel@graphmasters.net>
2020-06-07 15:10:43 +0200

    Refine A* Search

--- search.py
+++ search.py
@@ -19,6 +19,7 @@ Pacman agents (in searchAgents.py).
 
 import util
 
+
 class SearchProblem:
     """
     This class outlines the structure of a search problem, but doesn't implement
@@ -70,10 +71,10 @@ def tinyMazeSearch(problem):
     from game import Directions
     s = Directions.SOUTH
     w = Directions.WEST
-    return  [s, s, w, s, w, w, s, w]
+    return [s, s, w, s, w, w, s, w]
+
 
 def depthFirstSearch(problem: SearchProblem):
-
     def dfs(state: {int, int}, visited: [{int, int}] = []):
         if problem.isGoalState(state):
             return []
@@ -97,37 +98,39 @@ def depthFirstSearch(problem: SearchProblem):
     result = dfs(problem.getStartState())
     return result
 
+
 def breadthFirstSearch(problem: SearchProblem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
     util.raiseNotDefined()
 
+
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
     util.raiseNotDefined()
-    frontier = util.PriorityQueue()               # frontier is a Priority Queue (if every cost = 1, then FIFO)
-    frontier.push(problem.getStartState(), 0)    # add startState and its cost (0) to frontier
-    visited = []                                # list of states already visited (avoid loops)
+    frontier = util.PriorityQueue()  # frontier is a Priority Queue (if every cost = 1, then FIFO)
+    frontier.push(problem.getStartState(), 0)  # add startState and its cost (0) to frontier
+    visited = []  # list of states already visited (avoid loops)
     tempPath = []
-    path = []                                   # path to the goal (is returned)
-    pathToCurrent = util.PriorityQueue()               # paths to the states in the frontier
-    currState = frontier.pop()                  # current state is the first entry in frontier
+    path = []  # path to the goal (is returned)
+    pathToCurrent = util.PriorityQueue()  # paths to the states in the frontier
+    currState = frontier.pop()  # current state is the first entry in frontier
 
-    while not problem.isGoalState(currState):               # check whether current state is goal state
-        if currState not in visited:                        # skip visited states
-            visited.append(currState)                       # add current state to visted list
+    while not problem.isGoalState(currState):  # check whether current state is goal state
+        if currState not in visited:  # skip visited states
+            visited.append(currState)  # add current state to visted list
 
             successors = problem.getSuccessors(currState)
-            for (child, direction, cost) in successors:     # iterate through all children
+            for (child, direction, cost) in successors:  # iterate through all children
                 tempPath = path + [direction]
                 childCost = problem.getCostOfActions(tempPath)  # get cost to move to child
-                if child not in visited:                    # skip visited children
-                    frontier.push(child, childCost)         # add child and cost to frontier
+                if child not in visited:  # skip visited children
+                    frontier.push(child, childCost)  # add child and cost to frontier
                     pathToCurrent.push(tempPath, childCost)  # add path and cost to child to frontier
 
-        currState = frontier.pop()                          # set the next frontier entry as current state
-        path = pathToCurrent.pop()                          # set the path to the current State as path
+        currState = frontier.pop()  # set the next frontier entry as current state
+        path = pathToCurrent.pop()  # set the path to the current State as path
 
     return path
 
@@ -139,39 +142,53 @@ def nullHeuristic(state, problem=None):
     """
     return 0
 
-def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
-    visited = {}
-    queue = util.PriorityQueue()
-    def h(s):
-        return heuristic(s, problem)
 
-    # (cost, estimate, path, node)
-    estimate = h(problem.getStartState())
-    queue.push((0, estimate, [], problem.getStartState()), estimate)
+# Utility class to help structure the A* algorithm
+class Checkpoint:
+    def __init__(self, cost, path, node):
+        self.cost = cost
+        self.path = path
+        self.node = node
+
+
+def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
+    # utility method
+    def h(node):
+        return heuristic(node, problem)
+
+    visited = set()
+    queue = util.PriorityQueue()
+    queue.push(
+        item=Checkpoint(0, [], problem.getStartState()),
+        priority=0 + h(problem.getStartState())
+    )
 
     while not queue.isEmpty():
-        (pathcost, estimate, path, node) = queue.pop()
+        current: Checkpoint = queue.pop()
 
-        if node in visited and visited[node] <= estimate:
-            continue
-        else:
-            visited[node] = estimate
+        # if this is the goal node, we can just stop and return the path
+        if problem.isGoalState(current.node):
+            return current.path
 
-        if problem.isGoalState(node):
-            return path
+        # we can mark the current node as visited already. There can't be a smaller path from the current to the current
+        # since negative weights are not allowed
+        visited.add(current.node)
 
-        for (child, direction, cost) in problem.getSuccessors(node):
-            estimate = h(node)
-            queue.push((
-                pathcost + cost,
-                pathcost + cost + estimate,
-                path + [direction],
-                child
-            ), estimate)
+        # Iterate over every child node of current node, that we have not visited already
+        for (child, direction, cost) in problem.getSuccessors(current.node):
+            if child in visited:
+                continue
 
+            costToChild = current.cost + cost
+            pathToChild = current.path + [direction]
+
+            queue.update(
+                item=Checkpoint(costToChild, pathToChild, child),
+                priority=costToChild + h(child)
+            )
+
+    # No path from start to goal exists
     return None
-
-
 
 
 # Abbreviations
